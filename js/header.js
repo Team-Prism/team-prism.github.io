2f0a7896bb220b66999877144550dffa4f8e0f74
LeotomasMC <marycolageo@gmail.com>
2020-10-26 20:45:30 -0400

    cool stuff

--- js/header.js
+++ js/header.js
@@ -1,11 +1,60 @@
-function homePage() {
+//const body = document.getElementById("body")
 
+var heldKeys = {};
+window.onkeyup = (e) => { heldKeys[e.keyCode] = false; }
+window.onkeydown = (e) => { heldKeys[e.keyCode] = true; }
+
+let currentPage = "home"
+
+function homePage(smooth) {
+    currentPage = "home"
+    body.scrollTop = 0;
+    body.scrollTo({top: 0, left: body.clientWidth * 0, behavior: (smooth == true ? "smooth" : "auto")})
+    handleScroll()
+    //body.scrollLeft = body.clientWidth * 0;
 }
 
-function storyPage(pageId) {
-
+function storyPage(smooth, pageId) {
+    currentPage = "story"
+    body.scrollTop = 0;
+    body.scrollTo({top: 0, left: body.clientWidth * 1, behavior: (smooth == true ? "smooth" : "auto")})
+    handleScroll()
 }
 
-function aboutPage() {
+function aboutPage(smooth) {
+    currentPage = "about"
+    body.scrollTop = 0;
+    body.scrollTo({top: 0, left: body.clientWidth * 2, behavior: (smooth == true ? "smooth" : "auto")})
+    handleScroll()
+}
 
+function switchTheme() {
+    let style = document.getElementById("theme-css");
+    if (heldKeys[91] || heldKeys[16]) {
+        style.href = "./themes/very-dark.css"
+        localStorage.setItem("theme", "dark+");
+    } else {
+        if (style.href.endsWith("/dark.css")) {
+            style.href  = "./themes/light.css"
+            localStorage.setItem("theme", "light");
+            resetBackgroundImage("dark")
+        } else {
+            style.href  = "./themes/dark.css";
+            localStorage.setItem("theme", "dark");
+            resetBackgroundImage("dark")
+        }
+    }
+}
+
+function setThemeColor(c, skip) {
+    console.log(c)
+    c = {"r":"red","g":"green","b":"blue"}[c] || "dark";
+    if (!skip && document.getElementById("theme-css").href.endsWith("/themes/" + c + ".css")) {
+        console.log("reset theme")
+        c = "dark"
+    }
+    document.getElementById("theme-css").href = "./themes/" + c + ".css";
+    localStorage.setItem("theme", c);
+    if (c == "dark" || c == "light") resetBackgroundImage()
+    else document.getElementById("background").style.backgroundImage = 'url("./assets/bg-' + c + '.png")'
 }
--- js/index.js
+++ js/index.js
@@ -1,6 +1,59 @@
+var body;
+
+function resetBackgroundImage() {
+    document.getElementById("background").style.backgroundImage = (window.location.href.endsWith("meme") || (Math.random()*1000 < 5)  ? 'url("https://media1.tenor.com/images/9a762bc54a465df741b0efd0ac887601/tenor.gif")' : 'url("./assets/meadowgatedark.png")')
+}
+
 window.onload = function() {
     console.log("loaded page")
 
-    document.getElementById("background").style.backgroundImage = ((Math.random()*1000 < 5) ? 'url("https://media1.tenor.com/images/9a762bc54a465df741b0efd0ac887601/tenor.gif")' : 'url("./assets/temp-background.png")')
-    console.log(document.getElementById("background").style.backgroundImage)
+    body = document.getElementById("body")
+    
+    resetBackgroundImage()
+    handleScroll()
+    document.getElementById("home-content").onscroll = (e) => {setTimeout(() => {handleScroll()}, 0)}
+    document.getElementById("story-content").onscroll = (e) => {setTimeout(() => {handleScroll()}, 0)}
+    document.getElementById("about-content").onscroll = (e) => {setTimeout(() => {handleScroll()}, 0)}
+
+    document.getElementById("theme-css").href = "./themes/" + {"dark+":"very-dark","light":"light","dark":"dark","red":"red","green":"green","blue":"blue"}[localStorage.getItem("theme") || "dark"] + ".css";
+    if (["red","green","blue"].includes(localStorage.getItem("theme"))) {
+        setThemeColor({"red":"r","green":"g","blue":"b"}[localStorage.getItem("theme")], true);
+    }
+}
+
+/*function updateScrollbar() {
+    let list = document.getElementById("list-container")
+    let bar = document.getElementById("list-container-scrollbar")
+    bar.value = list.scrollTop;
+    bar.max = list.scrollHeight - list.clientHeight;
+    bar.hidden = (list.scrollHeight == list.clientHeight)
+}
+
+document.getElementById("full-content").addEventListener("scroll", updateScrollbar, {passive: true})
+
+document.getElementById("scrollbar").oninput = function(e) {
+    let bar = document.getElementById("scrollbar")
+    list.scrollTop = bar.value;
+    bar.max = list.scrollHeight - list.clientHeight;
+}*/
+
+function handleScroll() {
+    let el = document.getElementById(currentPage + "-content");
+    document.getElementById("scrollbar-handle").style.height = ((el.clientHeight / el.scrollHeight) * 100).toString() + "vh";
+    document.getElementById("scrollbar-handle").style.top = ((el.scrollTop / el.scrollHeight) * 100).toString() + "vh";
+    document.getElementById("scrollbar").style.display = (el.clientHeight < el.scrollHeight ? "block" : "none");
+    console.log(document.getElementById("scrollbar").style.display + " " + (el.clientHeight > el.scrollHeight));
+}
+
+window.onwheel = (e) => {
+    setTimeout(() => {document.getElementById(currentPage + "-content").scrollBy({top: e.deltaY * 2, behavior: "smooth"})}, 0)
+}
+
+window.ontouchstart = (e) => {
+    window.onwheel = null;
+    handleScroll = function() {}
+    document.getElementById("home-content").style.overflowY = "scroll";
+    document.getElementById("story-content").style.overflowY = "scroll";
+    document.getElementById("about-content").style.overflowY = "scroll";
+    window.ontouchstart = null;
 }
